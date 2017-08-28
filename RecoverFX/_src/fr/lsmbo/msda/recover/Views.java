1ddc8958d695a77f21bf3ce3711e3a32cf239a96
AlexandreBurel <AlexandreBurel@users.noreply.github.com>
2017-08-28 16:49:52 +0200

    removing useless folder

--- RecoverFX/_src/fr/lsmbo/msda/recover/Views.java
+++
@@ -1,9 +0,0 @@
-package fr.lsmbo.msda.recover;
-
-import java.net.URL;
-
-public class Views {
-
-	public final static URL RECOVER = Main.class.getResource("view/RecoverView.fxml");
-	public final static URL PARSING_RULES = Main.class.getResource("view/ParsingRulesView.fxml");
-}
