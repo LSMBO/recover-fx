84f722d6bd07f19d42fdbca619ab0c59b568a483
Romdhani <romdhani@live.fr>
2018-01-19 16:47:15 +0100

    [Recover-Gui] add asynchronous task executor .

---
+++ Recover/src/main/java/fr/lsmbo/msda/recover/view/executor/TaskExecutor.java
@@ -0,0 +1,127 @@
+package fr.lsmbo.msda.recover.view.executor;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Asynchronous task executor
+ * 
+ * @author aromdhani
+ *
+ */
+
+public class TaskExecutor {
+	private final Logger logger = LogManager.getLogger(TaskExecutor.class);
+	private final ExecutorService executorService = Executors.newCachedThreadPool();
+
+	private TaskExecutor() {
+	}
+
+	private static class Holder {
+		private final static TaskExecutor instance = new TaskExecutor();
+	}
+
+	public static TaskExecutor getInstance() {
+		return Holder.instance;
+
+	}
+
+	/**
+	 * execute a task
+	 * 
+	 * @param task
+	 */
+	public void execute(Runnable task) {
+		executorService.execute(createWrappedRunnableTask(task));
+	}
+
+	/**
+	 * execute task with delay
+	 * 
+	 * @param task
+	 * @param startTimout
+	 */
+	public void execute(Runnable task, long startTimout) {
+		executorService.execute(createWrappedRunnableTask(task));
+	}
+
+	/**
+	 * 
+	 * @param create
+	 *            <T> Callable<T> task
+	 * @return Callable task
+	 */
+	public static <T> Callable<T> creatCallable(final Callable<T> task) {
+		return () -> {
+			try {
+				return task.call();
+			} catch (Exception e) {
+				System.err.println("Error while trying to execute task ");
+				throw e;
+			}
+		};
+	}
+
+	/**
+	 * create Runnable task
+	 * 
+	 * @param task
+	 */
+	public Runnable createWrappedRunnableTask(final Runnable task) {
+		return () -> {
+			try {
+				task.run();
+			} catch (Exception e) {
+				System.err.println("Error while trying to execute runnable task");
+				throw e;
+			}
+		};
+	}
+
+	/**
+	 * submit task to executorService
+	 * 
+	 * @param task
+	 * @return
+	 */
+
+	public Future<?> submitRunnabletask(final Runnable task) {
+		return executorService.submit(createWrappedRunnableTask(task));
+	}
+
+	/**
+	 * submit Callable to executorService
+	 * 
+	 * @param task
+	 * @return
+	 */
+
+	public <T> Future<T> submitCallableTask(final Callable<T> task) {
+		return executorService.submit(creatCallable(task));
+	}
+
+	/**
+	 * shutdown executor of services
+	 */
+	public void close() {
+		try {
+			executorService.shutdown();
+			executorService.awaitTermination(5, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			System.err.println("Error while trying to shutdown executor service");
+		} finally {
+			if (!executorService.isTerminated()) {
+				System.err.println("Cancel non finished tasks");
+			}
+			executorService.shutdownNow();
+			System.out.println("shutdown of executorService.");
+		}
+
+	}
+}
