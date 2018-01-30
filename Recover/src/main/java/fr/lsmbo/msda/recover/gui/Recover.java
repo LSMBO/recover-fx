f35a8f291e1ae723f14a7151f8960e24adc432db
Romdhani <romdhani@live.fr>
2018-01-30 15:16:16 +0100

    [Recover-Gui] add initialize method and use one serviceExecutor

--- Recover/src/main/java/fr/lsmbo/msda/recover/gui/Recover.java
+++ Recover/src/main/java/fr/lsmbo/msda/recover/gui/Recover.java
@@ -1,18 +1,19 @@
 package fr.lsmbo.msda.recover.gui;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import fr.lsmbo.msda.recover.Main;
 import fr.lsmbo.msda.recover.Session;
 import fr.lsmbo.msda.recover.Views;
-import fr.lsmbo.msda.recover.task.TaskExecutor;
-import fr.lsmbo.msda.recover.view.panel.*;
 import fr.lsmbo.msda.recover.util.IconResource;
 import fr.lsmbo.msda.recover.view.RecoverController;
+import fr.lsmbo.msda.recover.view.component.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import fr.lsmbo.msda.recover.view.panel.MainPanel;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import fr.lsmbo.msda.recover.util.WindowSize;
@@ -30,7 +31,14 @@ public class Recover extends Application {
 
 	public static Boolean useSecondPeaklist = false;
 	public static Stage mainStage = null;
+	public static ExecutorService executor = null;
 	private static RecoverController recoverController = new RecoverController();
+
+	@Override
+	public void init() {
+		// initial first executor
+		executor = Executors.newWorkStealingPool();
+	}
 
 	public static void run() {
 		launch();
@@ -38,26 +46,18 @@ public class Recover extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
-		// window title
 		mainStage = primaryStage;
 		primaryStage.setTitle(Main.recoverTitle());
 		primaryStage.getIcons().add(IconResource.getImage(ICON.RECOVER));
-		// FXMLLoader loader = new FXMLLoader();
-		// loader.setLocation(Views.RECOVER);
-		// AnchorPane page = (AnchorPane) loader.load();
 		BorderPane page = MainPanel.getInstance();
 		Scene scene = new Scene(page);
 		primaryStage.setScene(scene);
-		// recoverController = loader.getController();
-		// recoverController.setDialogStage(primaryStage);
 		primaryStage.setWidth(WindowSize.prefWidth);
 		primaryStage.setMinWidth(WindowSize.minWidth);
 		primaryStage.setHeight(WindowSize.prefHeight);
 		primaryStage.setMinHeight(WindowSize.minHeight);
-		// display frame
 		primaryStage.show();
-
+		// new RecoverController();
 		// load files at the opening
 		if (Session.CURRENT_FILE != null) {
 			// recoverController.loadFile(Session.CURRENT_FILE);
@@ -72,7 +72,22 @@ public class Recover extends Application {
 
 	@Override
 	public void stop() {
-		// shutdown task executor
-		TaskExecutor.getInstance().close();
+		// shutdown Executor
+		close();
+	}
+
+	public void close() {
+		try {
+			executor.shutdown();
+			executor.awaitTermination(5, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			System.out.println("Error while trying to shutdown executor service");
+		} finally {
+			if (!executor.isTerminated()) {
+				System.out.println("Error Cancel non finished tasks");
+			}
+			executor.shutdownNow();
+			System.out.println("Info shutdown of ExecutorService.");
+		}
 	}
 }
