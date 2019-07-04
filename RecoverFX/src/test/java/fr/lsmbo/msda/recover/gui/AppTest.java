cc5fe43d23fe012632b55654917d846c537e496a
Romdhani <romdhani@live.fr>
2019-07-04 14:45:55 +0200

    [recover-fx]  add initial tests

---
+++ RecoverFX/src/test/java/fr/lsmbo/msda/recover/gui/AppTest.java
@@ -0,0 +1,69 @@
+package fr.lsmbo.msda.recover.gui;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+
+import org.junit.Assert;
+
+import fr.lsmbo.msda.recover.gui.io.PeaklistReader;
+import fr.lsmbo.msda.recover.gui.lists.ListOfSpectra;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static TestSuite suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	/**
+	 * Test Files
+	 */
+	public void testFile() {
+		try {
+			ClassLoader classLoader = getClass().getClassLoader();
+			File refSpectraFile = new File(classLoader.getResource("E_040566.mgf").getFile());
+			assertNotNull("The peaklist file", refSpectraFile.getName());
+			File testSpectraFile = new File(classLoader.getResource("E_040676.mgf").getFile());
+			assertNotNull("The peaklist file", testSpectraFile.getName());
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test the loaded spectra
+	 */
+	public void testSpectraLoder() {
+		// Load Spectra
+		try {
+			ClassLoader classLoader = getClass().getClassLoader();
+			File firstSpectraFile = new File(classLoader.getResource("E_040566.mgf").getFile());
+			assertNotNull("The peaklist file", firstSpectraFile.getName());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
